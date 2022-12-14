95c5e499a9f25268dfecb6b9e43688e15a8ffb88
Tachu <stitch_ditto_0j@icloud.com>
2022-12-14 18:18:12 +0900

    game

---
+++ practice/Playboy.java
@@ -0,0 +1,12 @@
+package practice;
+
+/**
+ * Playboy
+ */
+public class Playboy implements Character {
+
+    public int attack() {
+        System.out.println("遊び人は、ただ遊んでいるだけだった！");
+        return 0;
+    }
+}
---
+++ practice/Soldier.java
@@ -0,0 +1,12 @@
+package practice;
+
+/**
+ * Soldier
+ */
+public class Soldier implements Character {
+
+    public int attack() {
+        System.out.println("戦士の会心の一撃！");
+        return 100;
+    }
+}
