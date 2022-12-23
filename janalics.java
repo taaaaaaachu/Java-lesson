204ca06515a4a98a2324a2e9794521bcb07b5771
Tachu <stitch_ditto_0j@icloud.com>
2022-12-23 23:49:51 +0900

    commit

--- janalics.java
+++ janalics.java
@@ -3,11 +3,9 @@ import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
-Set<Integer> set = new LinkedHashSet<>();
-set.add(1); // OK
+Set<Integer>set=new LinkedHashSet<>();set.add(1); // OK
 set.add("Two"); // コンパイルエラー
 
-Map<Integer, String> map = new HashMap<>();
-map.put(1, "Taro"); // OK
-map.put(2, "Hanako"); // OK
-int value = map.get(0); // コンパイルエラー
+Map<Integer,String>map=new HashMap<>();map.put(1,"Taro"); // OK
+map.put(2,"Hanako"); // OK
+int value=map.get(0); // コンパイルエラー
