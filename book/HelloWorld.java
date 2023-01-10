04db675d56de54d1e736e3d862aa546ab52cbeed
Tachu <stitch_ditto_0j@icloud.com>
2023-01-10 14:31:23 +0900

    kyou

---
+++ book/HelloWorld.java
@@ -0,0 +1,12 @@
+package book;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("あほ");
+    }
+}
