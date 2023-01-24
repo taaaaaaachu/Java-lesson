65da17beb4f9781b9e907b77f8725a4c52292951
Tachu <stitch_ditto_0j@icloud.com>
2023-01-24 13:44:04 +0900

    f

--- book/Main.java
+++ book/Main.java
@@ -6,12 +6,16 @@ package book;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i < 16; i++) {
-            for (int j = 1; j < 16; j++) {
-                System.out.print(i * j);
-                System.out.print(" ");
-            }
-            System.out.println("");
+        int isHungry = 1;
+        String food = "meat";
+        System.out.println("こんにちは");
+        if (isHungry == 0) {
+            System.out.println("お腹いっぱいです");
+        } else {
+            System.out.println("はらぺこです");
+            System.out.println(food + "をいただきます");
         }
+        
+        System.out.println("ごちそうさまでした");
     }
 }
