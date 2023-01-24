4c98e6ecdbfb47267fe34e8b101132f9ac45da9a
Tachu <stitch_ditto_0j@icloud.com>
2023-01-24 14:28:42 +0900

    初期化

--- book/Main.java
+++ book/Main.java
@@ -6,16 +6,6 @@ package book;
 public class Main {
 
     public static void main(String[] args) {
-        int isHungry = 1;
-        String food = "meat";
-        System.out.println("こんにちは");
-        if (isHungry == 0) {
-            System.out.println("お腹いっぱいです");
-        } else {
-            System.out.println("はらぺこです");
-            System.out.println(food + "をいただきます");
-        }
         
-        System.out.println("ごちそうさまでした");
     }
 }
