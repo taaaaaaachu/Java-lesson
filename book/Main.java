62e51cca12cc7742c775ea7f369f485ff066eecc
Tachu <stitch_ditto_0j@icloud.com>
2023-01-24 13:09:32 +0900

    use i

--- book/Main.java
+++ book/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            System.out.println("こんにちは");
+            System.out.println("現在" + (i + 1) + "週目");
         }
     }
 }
