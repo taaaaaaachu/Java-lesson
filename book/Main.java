7643d11c819b921032cb316f4d1920481a09b14d
Tachu <stitch_ditto_0j@icloud.com>
2023-01-24 13:02:49 +0900

    あ

--- book/Main.java
+++ book/Main.java
@@ -16,6 +16,22 @@ public class Main {
         fortune ++;
         System.out.println("占いの結果が出ました");
         System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-        System.out.println("1:大吉 2:中吉 3:吉 4:凶");
+        switch (fortune) {
+            case 1:
+                System.out.println("大吉");
+                break;
+            case 2:
+                System.out.println("中吉");
+                break;
+            case 3:
+                System.out.println("吉");
+                break;
+            case 4:
+                System.out.println("凶");
+                break;
+            default:
+                
+                break;
+        }
     }
 }
