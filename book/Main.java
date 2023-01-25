0c551c47c835107b769c33e50559ee44cecbd6ea
Tachu <stitch_ditto_0j@icloud.com>
2023-01-25 12:58:49 +0900

    modon for

--- book/Main.java
+++ book/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         int[] scores = {20, 30, 40, 50, 80};
-        for (int i = 0; i < scores.length; i++) {
-            System.out.println(scores[i]);
+        for (int value : scores) {
+            System.out.println(value);
         }
     }
 } 
