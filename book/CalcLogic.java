5d4a96b14d16da8685922dcbd8e9c7f88d4e29dc
Tachu <stitch_ditto_0j@icloud.com>
2023-01-26 07:48:36 +0900

    local

---
+++ book/CalcLogic.java
@@ -0,0 +1,15 @@
+package book;
+
+/**
+ * CalcLogic
+ */
+public class CalcLogic {
+
+    public static int tasu(int a, int b) {
+        return (a + b);
+    }
+
+    public static int hiku(int a, int b) {
+        return (a - b);
+    }
+}
