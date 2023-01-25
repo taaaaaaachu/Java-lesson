9ec37d47069308bfaefb025c2c889400ea3961fc
Tachu <stitch_ditto_0j@icloud.com>
2023-01-25 12:55:37 +0900

    DNA

--- book/Main.java
+++ book/Main.java
@@ -6,6 +6,11 @@ package book;
 public class Main {
 
     public static void main(String[] args) {
-        
+        int[] seq = new int[10];
+        char[] base = {'A', 'T', 'G', 'C'};
+        for (int i = 0; i < seq.length; i++) {
+            seq[i] = new java.util.Random().nextInt(4);
+            System.out.print(base[seq[i]] + " ");
+        }
     }
 } 
