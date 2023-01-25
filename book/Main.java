3b2d1308059ee33fedf3abbe70bdd0f21ac19ad3
Tachu <stitch_ditto_0j@icloud.com>
2023-01-26 06:54:08 +0900

    あ

--- book/Main.java
+++ book/Main.java
@@ -4,16 +4,14 @@ package book;
  * Main
  */
 public class Main {
-    public static void methodA() {
-        System.out.println("methodA");
-        methodB();
-    }
-
     public static void main(String[] args) {
-        methodA();
+        System.out.println("メソッドを呼び出します");
+        hello("奏");
+        hello("浅香");
+        hello("菅原");
     }
 
-    public static void methodB() {
-        System.out.println("methodB");
+    public static void hello(String name) {
+        System.out.println(name + "さん、こんにちは");
     }
 }
