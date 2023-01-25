480c46c7336037fa8713b91a518c7e421a479fd5
Tachu <stitch_ditto_0j@icloud.com>
2023-01-26 06:56:29 +0900

    calc

--- book/Main.java
+++ book/Main.java
@@ -5,13 +5,12 @@ package book;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("メソッドを呼び出します");
-        hello("奏");
-        hello("浅香");
-        hello("菅原");
+        add(100, 20);
+        add(200, 50);
     }
 
-    public static void hello(String name) {
-        System.out.println(name + "さん、こんにちは");
+    public static void add(int x, int y) {
+        int ans = x + y;
+        System.out.println(x + "+" + y + "=" + ans);
     }
 }
